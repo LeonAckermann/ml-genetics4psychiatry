9de6775a635efb7b98c89cfa8e95188c0f8dec6f
Leon  ACKERMANN <leonmax.ackermann@gmail.com>
2026-03-12 16:29:59 +0100

    working TabPFN

---
+++ batch_scripts/tabpfn_mdd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --mem=100000
+#SBATCH -J tabpfn
+#SBATCH -o ../logs/tabpfn_%A_%a.out
+#SBATCH -e ../logs/tabpfn_%A_%a.err
+
+module load Python/3.12.9
+
+source ../pasteur/bin/activate
+export TABPFN_ALLOW_CPU_LARGE_DATASET=1
+
+#print working directory
+echo "Current working directory: $(pwd)"
+
+python3.12 ../scripts/pyscripts/tabpfn_mdd.py 
+
+#print done
+echo "TabPFN script completed successfully."
+
